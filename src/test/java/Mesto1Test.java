cb29399a2e9daaa14fe673a9e9d18eec7896c75c
ilchishinaea <ilchishina.e.a@gmail.com>
2025-11-27 20:54:31 +0300

    3

--- src/test/java/Mesto1Test.java
+++ src/test/java/Mesto1Test.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
 
 public class Mesto1Test {
 
